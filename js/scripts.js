526dfa6b09a38efb9decaebae2c02673702976e7
Luana Alves <luana.fsalves@gmail.com>
2024-11-29 15:09:13 +0000

    Password-generate-page

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Select of Elements
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatePasswordElement = document.querySelector("#generated-password");
+
+// Functions
+
+
+// 
+
+
+// Events
+generatePasswordButton.addEventListener("click", () => {
+    console.log("teste");
+});
