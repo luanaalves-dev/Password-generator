246dc0a43d9b51ed585b18abf793cb9cf4b1026e
Luana Alves <luana.fsalves@gmail.com>
2024-12-01 21:58:34 +0000

    password-functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,24 @@ const generatePasswordButton = document.querySelector("#generate-password");
 const generatePasswordElement = document.querySelector("#generated-password");
 
 // Functions
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
 
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
 
-// 
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+    const symbols = "(){} []=<>/,.!@#$%&*+-";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+console.log(getSymbol());
 
 
 // Events
